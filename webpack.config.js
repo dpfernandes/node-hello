633155ff774a2975ebfb62b9e7cdc13e80e0a26f
dpfernandes <daniel.pastor.fernandes@gmail.com>
2022-10-15 22:29:20 +0200

    adding webpack conf, manifest, favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,58 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './foo.js',
+  entry: ['babel-polyfill','index.js'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 100000 }
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    static: '/',
+    historyApiFallback: true,
+    port: parseInt(process.env.CLIENT_PORT, 10),
+    open: process.env.OPEN_BROWSER === 'true' ? true : false,
+    proxy: {
+      '/api': `http://localhost:${process.env.API_PORT}`,
+    },
+  },
+  node: {
+    global: false,
+    __filename: false,
+    __dirname: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new CaseSensitivePathsPlugin(),
+    new Dotenv({
+      safe: false,
+    }),
+  ],
 };
